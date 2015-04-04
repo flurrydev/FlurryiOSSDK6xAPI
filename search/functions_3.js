276bbcdeac0f2c56b06bcee1d02bc6f0029f4cef
Ugonna <mail@ugonna.co>
2015-04-03 17:42:20 -0700

    Updated documentation for SDK 6.3.0

---
+++ search/functions_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['displayadforspace_3amodallyforviewcontroller_3a',['displayAdForSpace:modallyForViewController:',['../interface_flurry_ads.html#a5de88e43799db193e3c1398a8af44763',1,'FlurryAds']]],
+  ['displayadforspace_3aonview_3aviewcontrollerforpresentation_3a',['displayAdForSpace:onView:viewControllerForPresentation:',['../interface_flurry_ads.html#a73e14f2cd53fd9e84c2a0a02c9e465ff',1,'FlurryAds']]],
+  ['displayadinview_3aviewcontrollerforpresentation_3a',['displayAdInView:viewControllerForPresentation:',['../interface_flurry_ad_banner.html#a855875abe2729b15446c71ddbd33a6d4',1,'FlurryAdBanner']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "_acdefghiklnprstuvw",
+  1: "f",
+  2: "f",
+  3: "_acdefgilprsv",
+  4: "aehklnrstuvw",
+  5: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "properties",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Properties",
+  5: "Pages"
+};
+
